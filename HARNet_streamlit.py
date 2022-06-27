bd06cf612976c759ca41dd94ca9c6dea7f4daa60
roderickperez <roderickperezaltamar@gmail.com>
2022-06-27 16:59:59 +0200

    new

--- HARNet_streamlit.py
+++ HARNet_streamlit.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 st.write("HARNet Project")
+
+st.set_page_config(page_title = "HARNet | Research Project", 
+                  page_icon = ":chart_with_upwards_trend:",
+                  layout = "wide")
+
+# ---- Header ----
+with st.container():
+    st.subheader("University of Vienna")
+    st.title("HARNet | Research Project")
+    st.write("Reference: [HARNet](https://arxiv.org/abs/1903.04909)")
+    
